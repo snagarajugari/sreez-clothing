5a042e8b054c3ed7b863ad16a659f3c2832f64b3
Sree Sai Chaitanya Nagarajugari <chaitunagarajugari@gmail.com>
2020-04-29 22:34:06 -0700

    Add routes to menu-items. Add placeholder code for hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./homepage";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
