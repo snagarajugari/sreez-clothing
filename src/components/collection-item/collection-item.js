6bc315f1fdead76e0fd6927734c9877dab05a180
Sree Sai Chaitanya Nagarajugari <chaitunagarajugari@gmail.com>
2020-05-14 10:44:55 -0700

    Add add-to-cart button on collection items

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-item.scss";
+import CustomButton from "../custom-button/custom-button";
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
   <div className="collection-item">
@@ -8,6 +9,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to Cart</CustomButton>
   </div>
 );
 
