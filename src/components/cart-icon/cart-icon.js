c9c49fbe6d26fdfbd1015e6e412d46cb50fa8609
Sree Sai Chaitanya Nagarajugari <chaitunagarajugari@gmail.com>
2020-05-16 20:58:47 -0700

    Add functionality to populate total items quantity in cart icon.

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -4,13 +4,20 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-icon.svg";
 import { connect } from "react-redux";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+const mapStateToProps = (state) => ({
+  itemCount: state.cart["cart-items"].reduce(
+    (accumlatedQuantity, cartItem) => accumlatedQuantity + cartItem.quantity,
+    0
+  ),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
