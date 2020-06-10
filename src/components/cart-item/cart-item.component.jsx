2f50105dfd5dc1a9b8b01f28bc4b2981b44ec130
Sree Sai Chaitanya Nagarajugari <chaitunagarajugari@gmail.com>
2020-06-09 23:29:08 -0700

    memoized cart-item component props to avoid the re-rendering  of all cart-items while they are  added to cart-dropdown component.

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import {
   NameContainer,
   ItemDetailsContainer,
@@ -17,4 +17,4 @@ const CartItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
   </CartItemContainer>
 );
 
-export default CartItem;
+export default memo(CartItem);
